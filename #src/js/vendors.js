67aa8d8b152cd495c170d96adadc751179f3db90
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-05-22 16:42:50 +0300

    current changes in the project structure

--- #src/js/vendors.js
+++
@@ -1,5 +0,0 @@
-@@include('libs/wNumb.min.js', {})
-@@include('libs/swiper.min.js', {})
-@@include('libs/nouislider.js', {})
-@@include('libs/smoothScroll.js', {})
-@@include('libs/inputmask.min.js', {})
