89e1e7da73d522c5ed9ee2dc3a6dc3af97013790
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-05-24 17:44:03 +0300

    added the button--up

--- #src/blocks/buttons/button.js
+++ #src/blocks/buttons/button.js
@@ -1,11 +1,22 @@
 (()=>{
   let employees = document.querySelectorAll('.button--employee');
-  employees.forEach(employee => {
-    employee.addEventListener('click', ()=>{
-      const text = employee.textContent;
-      employee.textContent = employee.dataset.text;
-      employee.dataset.text = text;
-      employee.dataset.sent = employee.dataset.sent ? employee.dataset.sent = 'false' : employee.dataset.sent = 'true'
+  if (employees.length) {
+    employees.forEach(employee => {
+      employee.addEventListener('click', () => {
+        const text = employee.textContent;
+        employee.textContent = employee.dataset.text;
+        employee.dataset.text = text;
+        employee.dataset.sent = employee.dataset.sent ? employee.dataset.sent = 'false' : employee.dataset.sent = 'true'
+      });
     });
-  });
+  };
+
+  let upBtn = document.querySelector('.button--up');
+  if (upBtn) {
+    upBtn.addEventListener('click', ()=>{
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"});
+    });
+  }
 })();
