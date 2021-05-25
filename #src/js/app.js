bf052c14fcde10d03e17b88f111a4e0d85771f30
Bogdanov Dmitry <b.demetrix@gmail.com>
2021-05-25 18:06:38 +0300

    finish commit

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,3 +1,4 @@
 @@include('files/regular.js', {})
 @@include('../blocks/buttons/button.js', {})
 @@include('../blocks/feedback/feedback.js', {})
+@@include('files/script.js', {})
---
+++ #src/js/files/script.js
@@ -0,0 +1,13 @@
+
+
+tippy('.confirmed', {
+  content: `<div class="confirmed-msg">
+							<div class="confirmed-msg__title">
+								Подтверждённая компания
+							</div>
+							<div class="confirmed-msg__text">
+								Реквизиты компании проверены банковским переводом.
+							</div>
+						</div>`,
+  allowHTML: true,
+});
